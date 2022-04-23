f946e96ede23a65b09feb6c870de6c45c6e0da8d
Admin <18130162@st.hcmuaf.edu.vn>
2022-04-23 21:28:53 +0700

    formlogin version3

--- src/screen/Login/FomLogin.js
+++ src/screen/Login/FomLogin.js
@@ -2,9 +2,35 @@
 function FormLogin(){
 
     return(
-        <div style={{width : '45%' , height : '100vh'}}>
+        <div className="container-form">
+        <h2>Chào mừng bạn đến với cửa hàng</h2>
+        <form>
+            <div className="field-email">
+                <input className="input-email" placeholder=" " type="text" name="email" id="email"/>
+                <label className="label-email" for="email">Email</label>
+                <i className="fa-solid fa-envelope-open"></i>
+                <span className="message-error">Vui lòng nhập email !</span>
+            </div>
+            <div className="field-pass">
+                <input className="input-pass" type="text" name="pass" id="pass" placeholder=" "/>
+                <label className="label-pass" for="pass">Mật khẩu</label>
+                <i className="fa-solid fa-user-secret"></i>
+                <span className="message-error">Trường này không được để trống !</span>
+            </div>
 
-        </div>
+            <div className="register-forgetpass">
+               <a href="#">Quên mật khẩu </a>
+               <a href="#">Bạn chưa có tài khoản?</a>
+            </div>
+            <div className="btn-login">
+                <button type="submit">
+                    {/* <i className="fa-solid fa-spinner fa-2x"></i>  */}
+                    Đăng nhập
+                </button>
+            </div>
+        </form>
+      
+    </div>
     )
 }
 export default FormLogin

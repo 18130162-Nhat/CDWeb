786dee9836edddf39b8939f190cd280a461e0304
Admin <18130162@st.hcmuaf.edu.vn>
2022-04-24 00:40:42 +0700

    add file FormError.js and write valid form

---
+++ src/Constant/ErrorForm.js
@@ -0,0 +1,44 @@
+
+const FIELD_EMPTY = 'FIELD_EMPTY'
+const REQUIRE_EMAIL = 'REQUIRE_EMAIL'
+const REQUIRE_PHONE = 'REQUIRE_PHONE'
+const TEXT_ERROR_EMPTY = "Trường này không được trống !"
+const TEXT_REQUIRE_EMAIL = "Trường này phải là email !"
+const TEXT_REQUIRE_PHONE = "Trường này phải là số điện thoại !"
+
+const getListError = (listNameError) =>{
+    let listError = []
+    listNameError.forEach(err => {
+        switch(err){
+            case FIELD_EMPTY:
+                let empty = (value) =>{
+                    return (value.trim().length===0)? TEXT_ERROR_EMPTY : undefined
+                }
+                listError.push(empty)
+            break ;
+            case REQUIRE_EMAIL:
+            let email = (value) =>{
+                let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+                    return regex.test(value.trim()) ? undefined : TEXT_REQUIRE_EMAIL
+            }
+            listError.push(email)
+            break ;
+
+            case REQUIRE_PHONE :
+               let phone = (value) =>{
+                let regex  = new RegExp('/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/')
+                return regex.test(value.trim()) ? undefined : TEXT_REQUIRE_PHONE
+               }
+
+            break ;
+
+            default : throw new Error("Name error invalid") 
+        }
+        
+    });
+    return listError
+
+}
+export {FIELD_EMPTY , REQUIRE_EMAIL , REQUIRE_PHONE , 
+        TEXT_ERROR_EMPTY ,TEXT_REQUIRE_EMAIL ,TEXT_REQUIRE_PHONE,
+        getListError}
--- src/screen/Login/FomLogin.js
+++ src/screen/Login/FomLogin.js
@@ -1,36 +1,102 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { FIELD_EMPTY, REQUIRE_EMAIL, getListError } from "../../Constant/ErrorForm"
 
-function FormLogin(){
+function FormLogin() {
 
-    return(
+    const [inputEmail, setInputEmail] = useState({
+        value: "", listError: getListError([FIELD_EMPTY, REQUIRE_EMAIL])
+        , messageError: " "
+    })
+
+    const [inputPass, setInputPass] = useState({
+        value: "", listError: getListError([FIELD_EMPTY])
+        , messageError: " "
+    })
+
+    const changEmail = (event) => {
+        setInputEmail({ ...inputEmail, value: event.target.value })
+    }
+    const changePass = (event) => {
+        setInputPass({ ...inputPass, value: event.target.value })
+    }
+    const blurEmail = () => {
+            validateEmail()
+    }
+    const blurPass = () => {
+        validatePass()
+
+    }
+    const validateEmail = () => {
+        let check = false
+        inputEmail.listError.forEach(func => {
+            if (func(inputEmail.value) === undefined) {
+                setInputEmail({ ...inputEmail, messageError: " " })
+                    check = true
+            }else {
+                setInputEmail({ ...inputEmail, messageError: func(inputEmail.value) })
+                check =false
+                return
+            }
+        })
+        return check
+    }
+
+    const validatePass = () =>{
+        let check = false
+        inputPass.listError.forEach(func =>{
+            if(func(inputPass.value)===undefined){
+                 setInputPass({...inputPass, messageError : " "})
+                 check = true
+            }
+            else {
+                setInputPass({...inputPass , messageError : func(inputPass.value)})
+               check = false 
+               return
+            }
+        })
+        return true
+    }
+    const submit = (event) =>{
+            event.preventDefault()
+            if(validateEmail() & validatePass()){
+              let formData = new FormData(event.currentTarget)
+              let email = formData.get("email")
+
+              let pass = formData.get("pass")
+             // API 
+            }
+    }
+    return (
         <div className="container-form">
-        <h2>Chào mừng bạn đến với cửa hàng</h2>
-        <form>
-            <div className="field-email">
-                <input className="input-email" placeholder=" " type="text" name="email" id="email"/>
-                <label className="label-email" for="email">Email</label>
-                <i className="fa-solid fa-envelope-open"></i>
-                <span className="message-error">Vui lòng nhập email !</span>
-            </div>
-            <div className="field-pass">
-                <input className="input-pass" type="text" name="pass" id="pass" placeholder=" "/>
-                <label className="label-pass" for="pass">Mật khẩu</label>
-                <i className="fa-solid fa-user-secret"></i>
-                <span className="message-error">Trường này không được để trống !</span>
-            </div>
+            <h2>Chào mừng bạn đến với cửa hàng</h2>
+            <form onSubmit={submit}>
+                <div className="field-email">
+                    <input onBlur={blurEmail} onChange={changEmail} className="input-email" placeholder=" " type="text" name="email" id="email" value={inputEmail.value} />
+                    <label className="label-email" for="email">Email*</label>
+                    <i className="fa-solid fa-envelope-open"></i>
+                    <span className="message-error">{inputEmail.messageError}</span>
+                </div>
+                <div className="field-pass">
+                    <input onBlur={blurPass} onChange={changePass} className="input-pass" type="password" name="pass" id="pass" placeholder=" " value={inputPass.value} />
+                    <label className="label-pass" for="pass">Mật khẩu*</label>
+                    <i className="fa-solid fa-user-secret"></i>
+                    <span className="message-error">{inputPass.messageError}</span>
+                </div>
 
-            <div className="register-forgetpass">
-               <a href="#">Quên mật khẩu </a>
-               <a href="#">Bạn chưa có tài khoản?</a>
-            </div>
-            <div className="btn-login">
-                <button type="submit">
-                    {/* <i className="fa-solid fa-spinner fa-2x"></i>  */}
-                    Đăng nhập
-                </button>
-            </div>
-        </form>
-      
-    </div>
+                <div className="register-forgetpass">
+                    <Link to={''}>Quên mật khẩu </Link>
+                    <Link to={''}>Bạn chưa có tài khoản?</Link>
+                </div>
+                <div className="btn-login">
+                    <button type="submit">
+                        {/* <i className="fa-solid fa-spinner fa-2x"></i>  */}
+                        Đăng nhập
+                    </button>
+                </div>
+            </form>
+
+        </div>
     )
 }
 export default FormLogin

31e33c15ea9a230d5b04c35611cf55af5720532a
Hoàng Vinh <73897533+18130284@users.noreply.github.com>
2022-07-15 23:53:07 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ import FormInformation from "./screen/Register/FormInFor";
 import FormTypeEmail from "./screen/Register/FormTypeEmail";
 import OTP from "./screen/Register/OTP";
 import PageRegister from "./screen/Register/PageRegister";
+import PageProfile from "./screen/Profile/PageProfile"
+import PageChangePass from "./screen/Profile/PageChangePass"
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <Layout/>
     // <Routes>
     //   < Route  element ={<Layout/>}>
@@ -19,6 +22,22 @@ function App() {
     //     </Route>
     //   </Route>
     // </Routes>
+=======
+    <Routes>
+      < Route  element ={<Layout/>}>
+        <Route path="/" index element ={<PageLogin/>}/>
+        <Route path="/register" element ={<PageRegister/>}>
+          <Route index path="formInfor" element={<FormInformation/>}/>
+          <Route path="formEmail" element ={<FormTypeEmail/>}/>
+          <Route path="OTP" element = {<OTP/>}/>
+        </Route>
+        <Route path="/pagecart"  element = {<PageCart/>}/>
+        <Route path="/pageprofile"  element = {<PageProfile/>}/>
+        <Route path="/pagechangepass"  element = {<PageChangePass/>}/>
+      </Route>
+    </Routes>
+   
+>>>>>>> Stashed changes
   );
 }
 

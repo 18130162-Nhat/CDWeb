bf38096d5f2567ffd05b6a4a832e55c14570bcd4
Admin <18130162@st.hcmuaf.edu.vn>
2022-07-19 18:24:00 +0700

    function login

--- src/service/Authen.js
+++ src/service/Authen.js
@@ -1,11 +1,21 @@
 
 
 const APIAuthen = {
-    signIn: async (sucess ,failure) =>{
-      setTimeout(() => {
-            sucess()
-        }, 4000);
-    
+    signIn: (sucess ,failure ,object ) =>{
+        fetch('http://localhost:8080/login' ,{
+            method : 'POST',
+            body : object
+        })
+        .then(res => {
+            if(!res.ok) throw new Error(res.status)
+            return res.json()
+        })
+        .then(data =>{
+            if(data.message==='oke') sucess()
+        })
+        .catch(err =>{
+            failure()
+        })
     },
     signOut : (sucess , failure) =>{
 
